78de7bbdcb328d50ab90afe388b6e4dd710dea1d
Kyle Dodson <kyledodson@gmail.com>
2019-03-18 22:42:06 -0700

    Initial project commit

---
+++ src/PushR.AspNetCore/PushRServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class PushRServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPushR(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            return services;
+        }
+    }
+}
